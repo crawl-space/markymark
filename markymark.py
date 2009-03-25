4974c0a196609b790b926b7138c36c5ca35d84f8
James Bowes <jbowes@dangerouslyinc.com>
2009-03-24 20:18:36 -0400

    initial commit

---
+++ markymark.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import curses
+
+curses.setupterm()
+
+color_normal = curses.tigetstr('sgr0')
+color_green = curses.tparm(curses.tigetstr('setaf'), curses.COLOR_GREEN)
+color_blue = curses.tparm(curses.tigetstr('setaf'), curses.COLOR_BLUE)
+color_magenta = curses.tparm(curses.tigetstr('setaf'), curses.COLOR_MAGENTA)
+color_red = curses.tparm(curses.tigetstr('setaf'), curses.COLOR_RED)
+
+bold = curses.tparm(curses.tigetstr('bold'), curses.A_BOLD)
+underline = curses.tparm(curses.tigetstr('smul'), curses.A_UNDERLINE)
+
+markup = {
+    'red' : color_red,
+    'green' : color_green,
+    'blue' : color_blue,
+    'magenta' : color_magenta,
+
+    'b' : bold,
+    'u' : underline,
+} 
+
+
+def convert(in_string):
+    out_string = in_string
+    for k, v in markup.items():
+        out_string = out_string.replace("[%s]" % k, v)
+        out_string = out_string.replace("[/%s]" % k, color_normal)
+
+    return out_string
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        print convert(" ".join(sys.argv[1:]))
+    else:
+        for line in sys.stdin:
+            sys.stdout.write(convert(line))
